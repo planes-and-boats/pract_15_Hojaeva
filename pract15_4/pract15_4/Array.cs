874b2e8b739078a5167b84dbd7a2a732da7f1a3b
shahodat <shahodat.hojaeva@gmail.com>
2026-04-25 10:24:45 +0500

    Практическая 15, вариант 4

---
+++ pract15_4/pract15_4/Array.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract15_4
+{
+    class Array
+    {
+        private int[] data;
+
+        public Array(int size)
+        {
+            data = new int[size];
+        }
+        public Array(int[] array)
+        {
+            data = array;
+        }
+        public void FullArray()
+        {
+            Random rnd = new Random();
+            data = Enumerable.Range(0, data.Length).Select(i => rnd.Next(1, 10)).ToArray();
+        }
+        public void Multiply(int factor)
+        {
+            data = data.Select(x => x * factor).ToArray();
+        }
+        public static Array Sum(Array a, Array b)
+        {
+            var resData = a.data.Zip(b.data, (x, y) => x + y).ToArray();
+            return new Array(resData);
+        }
+        public static Array Sub(Array a, Array b)
+        {
+            var resData = a.data.Zip(b.data, (x, y) => x - y).ToArray();
+            return new Array(resData);
+        }
+        public bool Search(int index, out int item)
+        {
+            if (index < 0 && index > data.Length - 1)
+            {
+                item = -1;
+                return false;
+            }
+            else
+            {
+                item = data[index];
+                return true;
+            }
+        }
+        public int Length => data.Length;
+        public void PrintAll() => Console.WriteLine(string.Join(" ", data));
+    }
+
+}
---
+++ pract15_4/pract15_4/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using pract15_4;
+namespace pract15_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ArrayList storage = new ArrayList();
+                while (true)
+                {
+                    Console.WriteLine($"\n--- МЕНЮ (Всего массивов: {storage.Count}) ---");
+                    Console.WriteLine("\n1. Создать массив\n2. Умножить на число\n3. Сложить массивы\n4. Вычесть массивы\n5. Показать все\n6. Найти элемент\n0. Выход");
+                    Console.Write("Выберите действие: ");
+                    string choice = Console.ReadLine();
+                    if (choice == "1")
+                    {
+                        Console.Write("Размер: ");
+                        int n = int.Parse(Console.ReadLine());
+                        Array arr = new Array(n);
+                        arr.FullArray();
+                        storage.Add(arr);
+                        Console.WriteLine("Массив создан и заполнен случайными числами.");
+                    }
+                    else if (choice == "2")
+                    {
+                        if (storage.Count == 0) { Console.WriteLine("Список пуст!"); continue; }
+
+                        int num;
+                        if (storage.Count == 1)
+                        {
+                            num = 1;
+                        }
+                        else
+                        {
+                            Console.Write($"Выберите номер массива (1-{storage.Count}): ");
+                            num = int.Parse(Console.ReadLine());
+                        }
+                        if (num > 0 && num <= storage.Count)
+                        {
+                            Console.Write("Множитель: ");
+                            int m = int.Parse(Console.ReadLine());
+                            ((Array)storage[num - 1]).Multiply(m);
+                            Console.WriteLine("Массив изменен.");
+                        }
+                        else Console.WriteLine("Неверный номер!");
+                    }
+                    else if (choice == "3" || choice == "4")
+                    {
+                        if (storage.Count < 2) { Console.WriteLine("Нужно минимум 2 массива!"); continue; }
+                        Console.Write($"Выберите номер массива (1-{storage.Count}): ");
+                        Console.Write("Номер массива 1: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.Write("Номер массива 2: ");
+                        int num2 = int.Parse(Console.ReadLine());
+
+                        if (num1 > 0 && num1 <= storage.Count && num2 > 0 && num2 <= storage.Count)
+                        {
+                            Array a = (Array)storage[num1 - 1];
+                            Array b = (Array)storage[num2 - 1];
+
+                            Array res = (choice == "3")
+                                ? Array.Sum(a, b)
+                                : Array.Sub(a, b);
+
+                            Console.Write("Результат: ");
+                            res.PrintAll();
+                        }
+                        else Console.WriteLine("Неверные номера!");
+                    }
+                    else if (choice == "5")
+                    {
+                        foreach (Array a in storage) a.PrintAll();
+                    }
+                    else if (choice == "6")
+                    {
+                        if (storage.Count == 0) { Console.WriteLine("Список пуст!"); continue; }
+
+                        Console.Write($"Номер массива (1-{storage.Count}): ");
+                        int num = int.Parse(Console.ReadLine());
+
+                        if (num > 0 && num <= storage.Count)
+                        {
+                            Console.Write($"Введите индекс (0-{((Array)storage[num - 1]).Length - 1}): ");
+                            int ind = int.Parse(Console.ReadLine());
+
+                            if (((Array)storage[num - 1]).Search(ind, out int item))
+                                Console.WriteLine("Элемент: " + item);
+                            else
+                                Console.WriteLine("Индекс вне диапазона!");
+                        }
+                    }
+                    else if (choice == "0") break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверный формат данных!");
+            }
+        }
+    }
+}
